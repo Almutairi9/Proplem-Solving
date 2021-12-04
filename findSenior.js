05c92861e3462248a76381fdee3e9a73b7fee8ab
Rawan Almutairi <rawan.fahad@tuwaiq.edu.sa>
2021-12-04 23:23:33 +0300

    Done !

---
+++ findSenior.js
@@ -0,0 +1,14 @@
+
+function findSenior(list) {
+  let oldest = 0;
+
+  for (let j = 0; j < list.length; j++) {
+    if (list[j].age > oldest) {
+      oldest = list[j].age;
+    }
+  }
+  const result = list.filter((ele) => {
+    return ele.age === oldest;
+  });
+  return result;
+}
