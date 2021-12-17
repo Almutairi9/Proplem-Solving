8e254b392d88da593b688537f69c64d57654c89b
Rawan Almutairi <rawan.fahad@tuwaiq.edu.sa>
2021-12-17 03:13:30 +0300

    Done 2 challenge

--- code_challenge/code-challenges/challenges-02.test.js
+++ code_challenge/code-challenges/challenges-02.test.js
@@ -24,6 +24,8 @@ Write a function that appends ' The end.' to a string, and returns the modified 
 
 const appendTheEnd = (str) => {
   // Solution code here...
+  let str1 = str;
+  return str1 + ' The end.' 
 };
 
 /* ------------------------------------------------------------------------------------------------
